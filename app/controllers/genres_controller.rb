e3f8c836cd52b1d3775ed8ec2e661c25db9fcc7d
Karen V <karenvo@usc.edu>
2019-12-02 12:28:32 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,22 @@
 class GenresController < ApplicationController
+  def create
+    
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+  
 end
